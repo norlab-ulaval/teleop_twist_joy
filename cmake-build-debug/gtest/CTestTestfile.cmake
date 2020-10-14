945dde0d1d6f080ab22945e9ca87f354f0a59556
DomBaril <dominic.baril@usherbrooke.ca>
2020-10-14 11:13:43 -0400

    removed debug junk

--- cmake-build-debug/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/dominic/catkin_ws/src/warthog_teleop_twist_joy/cmake-build-debug/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
